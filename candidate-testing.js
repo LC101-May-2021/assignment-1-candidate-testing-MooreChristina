951b5bde68addff79423e132693e3276f69af0c6
MooreChristina <>
2021-06-06 22:12:39 +0000

    asks questions, then loops to ask name and same questions again

--- candidate-testing.js
+++ candidate-testing.js
@@ -7,16 +7,17 @@ let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
 let correctAnswer;
-let candidateAnswer;
-let questions;
 let correctAnswers = [];
+let candidateAnswer;
+let questions = [];
+//let correctAnswers = [correctAnswer1, correctAnswer2, correctAnswer3, correctAnswer4, correctAnswer5];
 let candidateAnswers;
 
 let candidateLastName = "";
 let candidateFirstName = "";
 
 
-function askForName() {
+/*function askForName() {
   // TODO 1.1b: Ask for candidate's name //
      candidateFirstName = input.question("Enter your first name: ");
      candidateLastName =  input.question("Enter your last name: ");
@@ -24,50 +25,71 @@ function askForName() {
      return candidateName;
        
 }
+*/
 
+function askForName() {
+  // TODO 1.1b: Ask for candidate's name //
+let candidateFirstName = input.question("Please enter your first name: ", "<first name goes here>");
+let candidateLastName = input.question("Please enter your last name: ", "<last name goes here>");
+candidateName = (candidateFirstName + " " + candidateLastName);
+if (candidateName!= null) {
+
+   console.log("Hello " + candidateName + "! Good luck on the quiz.");
+
+}
+//
+}
 //question#1 info
-//const question1 = ("Who was the first American woman in space? ");
+let question1 = input.question("Who was the first American woman in space? ");
 //const correctAnswer1 = "Sally Ride";
 let candidateAnswer1 = "";
 
 //question# 2 info
-//const question2 = ("True or false: 5 kilometer == 5000 meters? ");
+let question2 = input.question("True or false: 5 kilometer == 5000 meters? ");
 const correctAnswer2 = "true";
 let candidateAnswer2 = "";
 
 //question#3 info
-//const question3 = ("(5 + 3)/2 * 10 = ? ");
+let question3 = input.question("(5 + 3)/2 * 10 = ? ");
 const correctAnswer3 = 40;
 let candidateAnswer3 = "";
 
 //question#4 info
-//const question4 = ("Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ");
+let question4 = input.question("Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ");
 const correctAnswer4 = "Trajectory";
 let candidateAnswer4 = "";
 
 //question#5 info
-//const question5 = ("What is the minimum crew size for the ISS? ");
+let  question5 = input.question("What is the minimum crew size for the ISS? ");
 const correctAnswer5 = 3;
 let candidateAnswer5 = "";
 
  
+ 
 
-function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer1 = input.question("Who was the first American woman in space? ");
-  candidateAnswer2 = input.question("True or false: 5 kilometer == 5000 meters? ");
-  candidateAnswer3 = input.question("(5 + 3)/2 * 10 = ? ");
-  candidateAnswer4 = input.question("Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ");
-  candidateAnswer5 = input.question("What is the minimum crew size for the ISS? ");
-candidateAnswer = [candidateAnswer1, candidateAnswer2, candidateAnswer3, candidateAnswer4, candidateAnswer5];
-}
+function askQuestion(){
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer 
+     candidateAnswer1 = input.question("1) Who was the first American woman in space? ");
+     candidateAnswer2 = input.question("2) True or false: 5 kilometer == 5000 meters? ");
+     candidateAnswer3 = input.question("3) (5 + 3)/2 * 10 = ? ");
+     candidateAnswer4 = input.question("4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ");
+     candidateAnswer5 = input.question("5) What is the minimum crew size for the ISS? ");
+  
+   // for (i = 0; i <= questions.length; i++);{
+	//	candidateAnswer = input(questions[i].question);
+  // }
+  return candidateAnswer = [candidateAnswer1, candidateAnswer2, candidateAnswer3, candidateAnswer4, candidateAnswer5];
+   }        
+ 
+//candidateAnswer = [candidateAnswer1, candidateAnswer2, candidateAnswer3, candidateAnswer4, candidateAnswer5];
+
 //let correctAnswer = [correctAnswer1, correctAnswer2, correctAnswer3, correctAnswer4, correctAnswer5];
 
 function gradeQuiz(candidateAnswers) {
   // console.log(correctAnswer); 
    //console.log(candidateAnswer);  
        
-   const correctAnswer1 = "Sally Ride";
+   correctAnswer1 = "Sally Ride";
    
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
@@ -84,12 +106,13 @@ function gradeQuiz(candidateAnswers) {
 
 function runProgram() {
   askForName();
+
   // TODO 1.1c: Ask for candidate's name //
-  console.log("Hello " + candidateName +"! " + "Good luck on your quiz. " );
   askQuestion();
+  
   gradeQuiz(this.candidateAnswers);
-    console.log(correctAnswer); 
-    console.log(candidateAnswer);
+  console.log (candidateName +"/n");
+    console.log(correctAnswer,candidateAnswer);
 }
 
 // Don't write any code below this line //
