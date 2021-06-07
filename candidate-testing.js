344940e141778a54533a0a2dab5abcc52be75c1b
MooreChristina <>
2021-06-07 04:10:24 +0000

    Start from here! output closer... but still not 100% complete. still in progess

--- candidate-testing.js
+++ candidate-testing.js
@@ -32,38 +32,39 @@ function askForName() {
 let candidateFirstName = input.question("Please enter your first name: ");
 let candidateLastName = input.question("Please enter your last name: ");
 candidateName = (candidateFirstName + " " + candidateLastName);
+return candidateName
 }
 
 //
 //question#1 info
 //let question1 = input.question("Who was the first American woman in space? ");
-//const correctAnswer1 = "Sally Ride";
+let correctAnswer1 = "Sally Ride";
 let candidateAnswer1 = "";
 
 //question# 2 info
 //let question2 = input.question("True or false: 5 kilometer == 5000 meters? ");
-const correctAnswer2 = "true";
+let correctAnswer2 = "true";
 let candidateAnswer2 = "";
 
 //question#3 info
 //let question3 = input.question("(5 + 3)/2 * 10 = ? ");
-const correctAnswer3 = 40;
+let correctAnswer3 = 40;
 let candidateAnswer3 = "";
 
 //question#4 info
 //let question4 = ("Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ");
-const correctAnswer4 = "Trajectory";
+let correctAnswer4 = "Trajectory";
 let candidateAnswer4 = "";
 
 //question#5 info
 //let  question5 = ("What is the minimum crew size for the ISS? ");
-const correctAnswer5 = 3;
+let correctAnswer5 = 3;
 let candidateAnswer5 = "";
 
  
  
 
-function askQuestion(){
+function askQuestion(candidateAnswers){
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer 
      question1 = input.question("1) Who was the first American woman in space? ");
          candidateAnswer1 = question1;
@@ -86,27 +87,44 @@ function askQuestion(){
  
 //candidateAnswer = [candidateAnswer1, candidateAnswer2, candidateAnswer3, candidateAnswer4, candidateAnswer5];
 
-//let correctAnswer = [correctAnswer1, correctAnswer2, correctAnswer3, correctAnswer4, correctAnswer5];
+correctAnswer = [correctAnswer1, correctAnswer2, correctAnswer3, correctAnswer4, correctAnswer5];
 
 function gradeQuiz(candidateAnswers) {
   // console.log(correctAnswer); 
    //console.log(candidateAnswer);  
-       
-   correctAnswer1 = "Sally Ride";
+ let grade1 = 0;
+ let grade2 = 0;  
+ let grade3 = 0;
+ let grade;
+ let numberCorrectAnswers;
+ let numberOfQuizQuestions = 3;
+   if (candidateAnswer1 === correctAnswer1){
+       grade1 = 1;
+          // else grade1 = 0}
+   } 
+   /*{
+    if (candidateAnswer2 === correctAnswer2)
+    {
+       grade2 = 1;
+    }
+       // else { grade2 = 0;}   
    
+   {
+    if (candidateAnswer3 === correctAnswer3)
+    {
+       grade3 = 1;
+    }
+   }
+   */
+   numberCorrectAnswers = Number(grade1);
+   //+ grade2 + grade3); // +grade4 + grade5);
+   grade = (Number((numberCorrectAnswers) / (numberOfQuizQuestions)) * 100);
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-
-
-  let grade = 0;
-      
-
-
-  
-
-  return grade;
+    console.log(grade);
 }
 
+ 
 function runProgram() {
   askForName();
   
@@ -116,8 +134,10 @@ function runProgram() {
   askQuestion();
   
   gradeQuiz(this.candidateAnswers);
+    
   console.log(candidateName);
-    console.log(correctAnswer,candidateAnswer);
+    console.log(questions, correctAnswer,candidateAnswer);
+   console.log(">>> Overall Grade: "); //+ grade +//"% ) ("numberCorrectAnswers + "of 5 responses correct)");
 }
 
 // Don't write any code below this line //
